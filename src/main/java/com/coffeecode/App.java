d6f16598588f35577b9136d9c7639e1b964b2a8a
Hasan Maki <42813480+nekadcoding@users.noreply.github.com>
2025-01-16 22:02:48 +0700

    Update project URL in pom.xml and simplify main method in App.java

--- src/main/java/com/coffeecode/App.java
+++ src/main/java/com/coffeecode/App.java
@@ -9,14 +9,7 @@ public class App {
 
     public static void main(String[] args) {
         logger.info("Hello World!");
-        logger.debug("This is a debug message");
-        logger.error("This is an error message");
-        logger.warn("This is a warn message");
-        logger.trace("This is a trace message");
-        try {
-            int unused = 10 / 0;
-        } catch (ArithmeticException e) {
-            logger.error("An exception occurred: ", e);
-        }
+        System.out.println("just starting the code");
+
     }
 }
